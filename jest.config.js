d77c9d2501b63c3215386950cf1a92810151276b
tja4472 <tja2353@gmail.com>
2022-02-10 11:16:22 +0000

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
+  /*
+    https://kulshekhar.github.io/ts-jest/docs/getting-started/paths-mapping/
+
+    tsconfig.json
+    "baseUrl": "src",    
+    "paths": {
+      "@app/*": ["app/*"]
+    }
+  */
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+  },
+};
